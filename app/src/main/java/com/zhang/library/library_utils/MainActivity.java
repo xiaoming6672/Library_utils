d92d688ba36f4ab943707ef0983a453186a51ebb
ZhangXiaoMing <zhangxiaoming6672@163.com>
2020-08-17 15:15:03 +0800

    GlideUtils test code

--- app/src/main/java/com/zhang/library/library_utils/MainActivity.java
+++ app/src/main/java/com/zhang/library/library_utils/MainActivity.java
@@ -1,8 +1,12 @@
 package com.zhang.library.library_utils;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.zhang.library.utils.utils.context.ContextUtils;
+import com.zhang.library.utils.utils.context.GlideUtils;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ContextUtils.set(getApplicationContext());
+
+        ImageView iv_image = findViewById(R.id.iv_image);
+
+        String url = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1597656168777&di=7690ab1d76a584ab9e02849bf1319a00&imgtype=0&src=http%3A%2F%2Ft8.baidu.com%2Fit%2Fu%3D3571592872%2C3353494284%26fm%3D79%26app%3D86%26f%3DJPEG%3Fw%3D1200%26h%3D1290";
+
+        GlideUtils.loadHead(R.drawable.ic_no_image, iv_image);
     }
 }
